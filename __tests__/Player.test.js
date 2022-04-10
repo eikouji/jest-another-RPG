5bcc9a9ba6884f185b56c201b5977ca3268a5875
Eiko Ujifusa <eikorouji@gmail.com>
2022-04-09 18:05:51 -0700

    added isAlive and reduceHealthto player.js file

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -33,3 +33,35 @@ test('gets inventory from player or returns false', () => {
   expect(player.getInventory()).toEqual(false);
 });
 
+
+// test to get information about player's health //
+test("gets player's health value", () => {
+  const player = new Player('Dave');
+
+  expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+// tests player alive? //
+test('checks if player is alive or not', () => {
+  const player = new Player('Dave');
+
+  expect(player.isAlive()).toBeTruthy();
+
+  player.health = 0;
+
+  expect(player.isAlive()).toBeFalsy();
+});
+
+// tests reduce health function // 
+test("subtracts from player's health", () => {
+  const player = new Player('Dave');
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+
+  expect(player.health).toBe(0);
+});
